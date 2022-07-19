47f7217fb445dc408f54f621bfda60fc1e41a4a9
Vitor Di Toro <vitorrditoro@gmail.com>
2022-07-19 01:07:25 -0300

    Create run2.py

---
+++ run2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import os
+
+N_EXECUTIONS = 1
+SEEDS = [1,2,3]
+RUNS = 50
+
+def execute_simulation(n_relays:int, seed:int, run:int):
+    os.system(f"python3 run -r {n_relays} -t {N_EXECUTIONS} -s {seed} -n {run}")
+
+
+
+for n_relays in range(5):
+    for seed in SEEDS:
+        for run in range(1,RUNS+1):
+            execute_simulation(n_relays, seed, run)
