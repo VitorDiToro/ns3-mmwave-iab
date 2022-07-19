d00e2af1109477b382b75da43032c787df3a2e75
Vitor Di Toro <vitorrditoro@gmail.com>
2022-07-19 01:07:19 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import re
 import sys
 import argparse
 from datetime import datetime
+import shutil
 
 
 def get_parameters():
@@ -26,20 +27,54 @@ def get_parameters():
                         action='store',
                         required=True,
                         help='Number of time to run the same simulation')
+    
+    parser.add_argument('-s',
+                        metavar='Seed',
+                        type=int,
+                        action='store',
+                        required=True,
+                        default=1,
+                        help='Random seed (Default: 1)') 
+      
+    parser.add_argument('-c',
+                        metavar='Schedule: RR/PF',
+                        type=str,
+                        action='store',
+                        required=False,
+                        choices=["RF", "rf", "PF", "pf"],
+                        default="RR",
+                        help='Type of schedule used on simulation. RR(Round Robin) or PF(Proportional Fair)')
+                            
+    parser.add_argument('-n',
+                        metavar='Execution number',
+                        type=int,
+                        action='store',
+                        required=True,
+                        default=0,
+                        help='Define execution RUN on RngSeedManager (Default: 0)') 
 
     args = parser.parse_args()
     current_dir_path = sys.argv[0].replace(sys.argv[0].split("/")[-1], '')
+    
+    n_relays = args.r
+    simulation_times = args.t
+    seed = args.s
+    schedule = args.c
+    run_turn = args.n
 
-    return [args.r, args.t, current_dir_path]
+    return [n_relays , simulation_times, seed, schedule, run_turn, current_dir_path]
 
 
-def create_path(current_dir: str, n_relays: int):
+def create_path(current_dir: str, n_relays: int, seed: int, run: int):
 
     path = f"{current_dir}results/{n_relays}-"
-    if n_relays == 1:
+    if n_relays in [0,1]:
         path += "relay/"
     else:
         path += "relays/"
+        
+    path += f"seed {seed}/"
+    path += f"run {run}/"
     path += datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
 
     if not os.path.exists(path):
@@ -51,12 +86,13 @@ def create_path(current_dir: str, n_relays: int):
 def execute_simulation():
     os.system("python2 waf configure --build-profile=debug --enable-examples")
     os.system("python2 waf")
+    
     os.system("python2 waf --run mmwave-iab-grid")
 
 
 def move_results(base_path: str, execution_time: int):
     files_list = ["DataRadioBearerCreatedTrace.txt",
-                  "EN-UE.txt",
+                  "ENB-UE.txt",
                   "RlcAmBufferSize.txt",
                   "RxPacketTrace.txt",
                   "StateTransitionTrace.txt",
@@ -71,7 +107,8 @@ def move_results(base_path: str, execution_time: int):
 
     for file in files_list:
         print(f"Moved {file}, To {folder_path}")
-        os.system(f"mv {file} {folder_path}")
+        shutil.move(file, folder_path)
+        # os.system(f"mv {file} {folder_path}")
 
 
 def update_n_relays_in_source_code(current_dir:str, n_relays: int):
@@ -86,12 +123,40 @@ def update_n_relays_in_source_code(current_dir:str, n_relays: int):
         file.write(new_content)
         file.truncate()
 
+def update_seed_in_source_code(current_dir:str, seed: int):
+    file_name = "mmwave-iab-grid.cc"
+    file_path = f"{current_dir}scratch/{file_name}"
+    with open(file_path, 'r+') as file:
+        content = file.read()
+        
+        new_content = re.sub(r'(RngSeedManager::SetSeed)\s*\(\d\)',
+                             r'\1({n})'.format(n=seed),
+                             content, flags=re.M)
+        file.seek(0)
+        file.write(new_content)
+        file.truncate()
+
+def update_run_turn_in_source_code(current_dir:str, run: int):
+    file_name = "mmwave-iab-grid.cc"
+    file_path = f"{current_dir}scratch/{file_name}"
+    with open(file_path, 'r+') as file:
+        content = file.read()
+        
+        new_content = re.sub(r'(unsigned run)\s*=\s*\d',
+                             r'\1 = {n}'.format(n=run),
+                             content, flags=re.M)
+        file.seek(0)
+        file.write(new_content)
+        file.truncate()
+
 
 if __name__ == '__main__':
-    n_relays, n_executions, current_dir = get_parameters()
-    output_base_path = create_path(current_dir, n_relays)
+    n_relays, n_executions, seed, schedule_type, run_number, current_dir = get_parameters()
+    output_base_path = create_path(current_dir, n_relays, seed, run_number)
     update_n_relays_in_source_code(current_dir, n_relays)
-
+    update_seed_in_source_code(current_dir, seed)
+    update_run_turn_in_source_code(current_dir, run_number)
+    
     for execution in range(1, n_executions+1):
         execute_simulation()
         move_results(output_base_path, execution)
